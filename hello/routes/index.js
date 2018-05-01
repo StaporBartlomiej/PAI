a920ba19ec4d1161d2b21925903c4b2b0ace7f44
Bartek <bartlomiej.stapor@gmail.com>
2018-05-02 00:38:07 +0200

    Trying to figure out how to get price from db

--- hello/routes/index.js
+++ hello/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
+var DateDiff = require('date-diff');
 
 var db = mysql.createConnection({
     host: 'localhost',
@@ -55,40 +56,62 @@ router.get('/admin', function (req,res) {
 });
 
 router.post('/reserveResult', function (req,res) {
+    var book_in_place = req.body.book_in_place;
+    var book_in_date = req.body.book_in_date;
+    var book_in_time = req.body.book_in_time;
+    var book_out_place = req.body.book_out_place;
+    var book_out_date = req.body.book_out_date;
+    var book_out_time = req.body.book_out_time;
+    var consumer_name = req.body.consumer_name;
+    var consumer_surname = req.body.consumer_surname;
+    var consumer_email = req.body.consumer_email;
+    var consumer_phone = req.body.consumer_phone;
+    var chosen_car = req.body.chosen_car;
+    var price;
+    console.log(book_in_place);
+    console.log(book_in_date);
+    console.log(book_in_time);
+    console.log(book_out_place);
+    console.log(book_out_time);
+    console.log(book_in_date);
+    console.log(consumer_name);
+    console.log(consumer_surname);
+    console.log(consumer_email);
+    console.log(consumer_phone);
+    console.log(chosen_car);
+
+    var book_in_date_converted_to_js_format = new Date(book_in_date);
+    var book_out_date_converted_to_js_format = new Date(book_out_date);
+    var car_price_query = "Select price_per_day from cars where car_name='" + chosen_car + "';";
+    console.log(car_price_query);
+    var myCar = new Object();
+
+    var dupa = db.query(car_price_query,function (error,result) {
+        console.log("price from db:" + result[0].price_per_day);
+        myCar.make = result[0].price_per_day;
+        return result[0].price_per_day;
+    });
+    console.log("dupa: " + dupa);
+    var total_days_car_is_rented = new DateDiff(book_out_date_converted_to_js_format, book_in_date_converted_to_js_format);
+    // var price =
+
+
+    var query = "Insert into table reservations(consumer_name,consumer_surname,id_card_number,consumer_email,consumer_phone,book_in_date," +
+        "book_out_date, total_price)";
+
     res.render('reserveResult', {title: "Reservation Details"});
 
 });
 
 function parseMysqlDateToJsDate(date) {
+    console.log("Date in func:" + date);
     var a=date.split(" ");
     var d=a[0].split("-");
     var t=a[1].split(":");
-    var formatedDate = new Date(d[0],(d[1]-1),d[2],t[0],t[1],t[2]);
-    return formatedDate;
+    return new Date(d[0], (d[1] - 1), d[2], t[0], t[1], t[2]);
 }
 router.get('/reserve', function (req,res) {
-    // var book_in_place = req.body.book_in_place;
-    // var book_in_date = req.body.book_in_date;
-    // var book_in_time = req.body.book_in_time;
-    // var book_out_place = req.body.book_out_place;
-    // var book_out_date = req.body.book_out_date;
-    // var book_out_time = req.body.book_out_time;
-    // var consumer_name = req.body.consumer_name;
-    // var consumer_surname = req.body.consumer_surname;
-    // var consumer_email = req.body.consumer_email;
-    // var consumer_phone = req.body.consumer_phone;
-    // var temp;
-    // temp = parseMysqlDateToJsDate(book_in_date);
-    // book_in_date = temp;
-    // temp = parseMysqlDateToJsDate(book_out_date);
-    // book_out_date = temp;
-    //  //returned from mysql timestamp/datetime field
-    //
-    //
-    // var timeDifference = Math.abs(book_out_date.getTime() - book_in_date.getTime());
-    // var differentDays = Math.ceil(timeDifference / (1000 * 3600 * 24));
-    // var query = "Insert into table reservations(consumer_name,consumer_surname,id_card_number,consumer_email,consumer_phone,book_in_date," +
-    //     "book_out_date, total_price)";
+
 
 
 
