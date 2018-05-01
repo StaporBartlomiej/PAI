8d7dffde2a1fd3e90cc5908d71dc91f7661ea4bb
elec <bartlomiej.stapor@gmail.com>
2018-05-01 17:22:05 +0200

    Added switching throug views

--- hello/routes/index.js
+++ hello/routes/index.js
@@ -9,33 +9,48 @@ var mysql = require('mysql');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var test = 'huehue';
-  var query = 'select * from country;';
-  var db = mysql.createConnection({
-    host: '172.30.24.12',
-    user: 'dzida_1119580',
-    password: '58118057',
-    database: 'DZIDA'
-    
-    });
-    db.connect();
+  // var test = 'huehue';
+  // var query = 'select * from country;';
+  // var db = mysql.createConnection({
+  //   host: '172.30.24.12',
+  //   user: 'dzida_1119580',
+  //   password: '58118057',
+  //   database: 'DZIDA'
+  //
+  //   });
+  //   db.connect();
     //res.render('index', { title: 'Express'});
+    // db.query(query, function(error, dane) {
+    //     res.render('index', {title: 'Express', dane: dane});
+    // });
+    res.render('index', {title: 'Express'});
+});
+
+router.get('/home', function(req, res, next) {
+    res.render('index', { title: 'Home' });
+});
+
+router.get('/flota', function (req,res) {
+    var test = 'huehue';
+    var query = 'select * from cars;';
+    var db = mysql.createConnection({
+      host: '172.30.24.12',
+      user: 'dzida_1119580',
+      password: '58118057',
+      database: 'DZIDA'
+
+      });
+      db.connect();
+    res.render('index', { title: 'Express'});
     db.query(query, function(error, dane) {
         res.render('index', {title: 'Express', dane: dane});
     });
-});
-
-router.get('/cos', function(req, res, next) {
-    res.render('index2', { title: 'Bartek' });
-});
-
-router.post('studentuj', function (req,res) {
-    res.render('index3', {title: req.body.student});
+    res.render('index2', {title: 'flota'});
 
 });
 
-router.post('/register', function (req,res) {
-    res.render('index2', {title: "test"});
+router.get('/admin', function (req,res) {
+    res.render('index3', {title: "Admin Panel"});
 
 });
 
